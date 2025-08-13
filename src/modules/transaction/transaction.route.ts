749ead9ab64d7a1a6067b6f07bec6a55213d0aac
nahidn228 <nahidn228@gmail.com>
2025-08-13 22:36:55 +0600

    add transaction zod schema in transaction Route

--- src/modules/transaction/transaction.route.ts
+++ src/modules/transaction/transaction.route.ts
@@ -1,18 +1,34 @@
 import { Router } from "express";
 import { TransactionController } from "./transaction.controller";
+import { validateRequest } from "../../middleware/validateRequest";
+import { transactionZodSchema } from "./transaction.validate";
 
 const transactionRoutes = Router();
 
-transactionRoutes.post("/deposit", TransactionController.deposit);
-transactionRoutes.post("/withdraw", TransactionController.withdraw);
-transactionRoutes.post("/sendMoney", TransactionController.sendMoney);
+transactionRoutes.post(
+  "/deposit",
+  validateRequest(transactionZodSchema.depositSchema),
+  TransactionController.deposit
+);
+transactionRoutes.post(
+  "/withdraw",
+  validateRequest(transactionZodSchema.withdrawSchema),
+  TransactionController.withdraw
+);
+transactionRoutes.post(
+  "/sendMoney",
+  validateRequest(transactionZodSchema.sendMoneySchema),
+  TransactionController.sendMoney
+);
 transactionRoutes.get(
   "/history/:walletId",
+  validateRequest(transactionZodSchema.getTransactionHistorySchema),
   TransactionController.getTransactionHistory
 );
 
 transactionRoutes.patch(
   "/status/:id",
+  validateRequest(transactionZodSchema.changeTransactionStatusSchema),
   TransactionController.changeTransactionStatus
 );
 
