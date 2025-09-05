cb27715f223267c21d5859a6db0c0fafd734550e
nahidn228 <nahidn228@gmail.com>
2025-09-06 01:12:32 +0600

    Remove dist folder from repository

--- dist/modules/user/user.constrain.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserRole = void 0;
-var UserRole;
-(function (UserRole) {
-    UserRole["Admin"] = "Admin";
-    UserRole["User"] = "User";
-    UserRole["Agent"] = "Agent";
-})(UserRole || (exports.UserRole = UserRole = {}));
--- dist/modules/wallet/wallet.constrain.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WalletStatus = void 0;
-var WalletStatus;
-(function (WalletStatus) {
-    WalletStatus["ACTIVE"] = "Active";
-    WalletStatus["BLOCKED"] = "Blocked";
-    WalletStatus["SUSPENDED"] = "Suspended";
-})(WalletStatus || (exports.WalletStatus = WalletStatus = {}));
